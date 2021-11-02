21c5540c0a179d7bd7cd7056f15f7a63a40c398d
alexmcd223 <65975621+alexmcd223@users.noreply.github.com>
2021-11-02 22:16:10 +0000

    Update createMessages.js
    Created Messages table

--- sql/createMessages.js
+++ sql/createMessages.js
@@ -5,7 +5,13 @@
 // Repeat the setup for “CURRENT_TIMESTAMP” as outlined in the previous requirement.
 
 const createMessages = `
-
-
+    CREATE TABLE IF NOT EXISTS Messages(
+        id INTEGER NOT NULL PRIMARY KEY,
+        userid INTEGER NOT NULL,
+        message TEXT NOT NULL,
+        created INTEGER NOT NULL DEFAULT (strftime('%s','now')), 
+        archive INTEGER(1) DEFAULT 0 CHECK (archive = 1 or archive = 0),
+        FOREIGN KEY (userid) REFERENCES Users(userid) ON DELETE CASCADE
+    );
 `
 module.exports = { createMessages };
