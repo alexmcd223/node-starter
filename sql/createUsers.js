3c782ec5ebd95d915b1aa3b50726fd2a865d9de7
alexmcd223 <65975621+alexmcd223@users.noreply.github.com>
2021-11-02 20:46:34 +0000

    created table in createUsers
    Completed task1

--- sql/createUsers.js
+++ sql/createUsers.js
@@ -3,8 +3,15 @@
 // in the creation (IF NOT EXISTS) of the ‘Users’ table as illustrated in
 // the AdaBoard schema; don’t forget to include the Primary Key and unique constraints.
 // Use “strftime('%s','now')” rather than “CURRENT_TIMESTAMP”.
+
 const createUsers =`
-
-
-`
+    CREATE TABLE IF NOT EXISTS users(
+      userid INTEGER NOT NULL PRIMARY KEY,
+      friendlyname VARCHAR(50),
+      emailaddress VARCHAR(320) UNIQUE,
+      password VARCHAR(256),
+      admin INTEGER(1) NOT NULL CHECK (admin = 1 OR admin = 0),
+      lastlogin INTEGER NOT NULL DEFAULT (strftime('%s','now'))
+    );
+`;
 module.exports = { createUsers };
